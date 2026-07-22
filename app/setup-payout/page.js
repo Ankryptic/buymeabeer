0aa06a588ca5a43e8eae1946629e7169df27c037
Ankit soni <asoni.soni12@gmail.com>
2026-07-22 18:58:56 +0530

    add Skip button logic

--- app/setup-payout/page.js
+++ app/setup-payout/page.js
@@ -36,9 +36,10 @@ const countryList = [
 // Setup Payout
 const SetupPayout = () => {
     const [option, setOption] = useState(false);
-    const [country, setCountry] = useState("Choose your country")
+    const [country, setCountry] = useState("")
     const [searchRes, setSearchRes] = useState('')
     const searchInput = useRef(null)
+    const [afterSkip, setAfterSkip] = useState(false)
 
 
     // Filters the Country List
@@ -47,7 +48,7 @@ const SetupPayout = () => {
         return val.country.toLowerCase().includes(query);
     });
 
-    
+
     const handleOption = () => {
         setOption(!option)
     }
@@ -59,6 +60,10 @@ const SetupPayout = () => {
 
     const handleInputFocus = () => {
         searchInput.current.focus()
+    }
+
+    const handleSkip = () => {
+        setAfterSkip(true)
     }
 
     return (
@@ -94,7 +99,7 @@ const SetupPayout = () => {
                                     height={16}
                                     alt="globe"
                                 />
-                                <span>{country}</span>
+                                <span>{country || "Choose your country"}</span>
                             </div>
                             <div>
                                 <Image
@@ -107,7 +112,7 @@ const SetupPayout = () => {
                             </div>
                         </div>
 
-                        {/* Country Options  */}
+                        {/* Country Options DropDown */}
                         <div className={`absolute top-20 z-10 Countr-list w-135 rounded-2xl overflow-x-hidden overflow-y-auto scrollbar-none bg-[#3b354f] transition-all duration-300 ${option ? "opacity-100 h-100" : "opacity-0 h-0"} pb-10`} >
 
                             <div className="bg-[#3b354f] w-full sticky top-0 p-4">
@@ -133,6 +138,21 @@ const SetupPayout = () => {
                             </div>
                         </div>
 
+                        {country && <div className="flex gap-2 bg-[#2f2d41] p-4 rounded-xl">
+                            <Image
+                                className="bg-green-600 rounded-full h-fit mt-1"
+                                src={"/tick.svg"}
+                                width={14}
+                                height={14}
+                                alt="tick-logo"
+                            />
+                            <div className="text-sm">Good news - we support bank deposits in the {country}, thanks to our partner.</div>
+                        </div>}
+
+                        {country && <div className="w-full flex items-center justify-end">
+                            <Link href={"/setup-payout"} className="text-center w-full cursor-pointer bg-[#181921] hover:bg-[#0d0d12] py-4 px-12 rounded-full">Connect</Link>
+                        </div>}
+
                         <div className="flex">
                             <Image
                                 className="h-fit"
@@ -147,6 +167,41 @@ const SetupPayout = () => {
                 </div>
 
             </div>
+            
+            {/* Pop-up after skip  */}
+           {afterSkip && <div className="fixed top-0 z-20 h-screen w-full flex items-center justify-center bg-[#0000006a] text-white">
+
+                <div className="pop-box bg-[#2f2d41] w-120 h-80 py-4 px-6 rounded-xl shadow-2xl shadow-black">
+                    <div className="flex justify-between">
+                        <div className="relative">
+                            <Image
+                                className="invert"
+                                src={"/rupee.svg"}
+                                width={60}
+                                height={60}
+                                alt="rupee-logo"
+                            />
+                            <Image
+                                className="invert absolute right-1 bottom-1"
+                                src={"/danger.svg"}
+                                width={24}
+                                height={24}
+                                alt="danger-logo"
+                            />
+                        </div>
+                        <div className="text-white text-xl h-fit cursor-pointer hover:text-red-600" onClick={() => setAfterSkip(false)}>&#10008;</div>
+                    </div>
+
+                    <h2 className="text-2xl font-bold mt-6">Before you continue...</h2>
+                    <h4 className="text-gray-300 mt-2">Your page won't be able to accept payment until you connect a payout method. You can still setup your page and publish posts.</h4>
+
+                    <div className="buttons flex items-center my-6 gap-2">
+                        <button className="text-center text-sm w-full cursor-pointer bg-[#181921] hover:bg-[#0d0d12] py-4 px-12 rounded-full" onClick={() => setAfterSkip(false)}>Connect</button>
+                        <Link href={"/"} className="text-center text-sm w-full cursor-pointer bg-[#5b5570] hover:bg-[#3b354f] py-4 px-6 rounded-full">Got it! I'll do this later</Link>
+                    </div>
+                </div>
+
+            </div>}
 
             <div className="section-2 absolute bottom-0 h-26 w-full ">
                 <div className="bar w-full flex gap-3">
@@ -155,7 +210,7 @@ const SetupPayout = () => {
                     <div className="bar-1 h-1 w-full bg-black"></div>
                 </div>
                 <div className="w-full flex items-center justify-end my-5 pr-15">
-                    <Link href={"/setup-payout"} className="text-center text-white cursor-pointer underline hover:no-underline my-4 mr-4">Skip this setup</Link>
+                    <button className="text-center text-white cursor-pointer underline hover:no-underline my-4 mr-4" onClick={handleSkip}>Skip this setup</button>
                 </div>
             </div>
         </div>
